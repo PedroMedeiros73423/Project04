3c6e1603a82424e5d4f7817f94565536a2313313
PedroMedeiros7631 <81970006+PedroMedeiros73423@users.noreply.github.com>
2024-09-21 14:31:38 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/project04/Project04.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.project04;
+
+public class Project04 {
+
+    public static void main(String[] args) {
+        String nome;
+        nome = "João";
+        System.out.println(nome);
+        nome = "Maria";
+        System.out.println("O valor da Variável é :" + nome);
+    }
+}
